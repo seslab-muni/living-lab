e9264f124c60d759c194c3fd80cdc18be4138870
Patrik Prochazka <me@patrikprochazka.com>
2025-03-15 10:56:02 +0100

    remove unused import

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { validateDatabaseEnvironment } from './utils/environment';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
