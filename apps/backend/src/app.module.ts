84c4b413c3d02dc467e7281876e126fb2fa3f966
Patrik Prochazka <me@patrikprochazka.com>
2025-03-15 10:52:45 +0100

    update config for local dev

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import databaseDevConfig from './configuration/database-dev.config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(databaseDevConfig)],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot(databaseDevConfig),
+  ],
   controllers: [],
   providers: [],
 })
--- apps/backend/src/configuration/database-dev.config.ts
+++ apps/backend/src/configuration/database-dev.config.ts
@@ -2,12 +2,12 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export function getTypeOrmConfig(): TypeOrmModuleOptions {
   return {
-    type: process.env.DATABASE_TYPE as any,
-    host: process.env.DATABASE_HOST,
+    type: process.env.DATABASE_TYPE || ('postgres' as any),
+    host: process.env.DATABASE_HOST || '127.0.0.1',
     port: parseInt(process.env.DATABASE_PORT || '5432', 10),
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USERNAME || 'admin',
+    password: process.env.DATABASE_PASSWORD || 'adminpassword',
+    database: process.env.DATABASE_NAME || 'bvv-dev',
     autoLoadEntities: true,
     synchronize: process.env.NODE_ENV !== 'production',
   };
