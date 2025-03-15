e49a5a572ebe2f1e04dfd1a30d8d2502379defad
Patrik Prochazka <me@patrikprochazka.com>
2025-03-15 10:56:25 +0100

    ignore eslint error for any in db config

--- apps/backend/src/configuration/database-dev.config.ts
+++ apps/backend/src/configuration/database-dev.config.ts
@@ -2,6 +2,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export function getTypeOrmConfig(): TypeOrmModuleOptions {
   return {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     type: process.env.DATABASE_TYPE || ('postgres' as any),
     host: process.env.DATABASE_HOST || '127.0.0.1',
     port: parseInt(process.env.DATABASE_PORT || '5432', 10),
