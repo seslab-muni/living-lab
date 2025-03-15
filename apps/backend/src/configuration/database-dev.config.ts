a403c5a3767068850a11c2e645ad0a5c578f466b
Patrik Procházka <me@patrikprochazka.com>
2025-03-15 11:10:32 +0100

    Add database support (#2)

---
+++ apps/backend/src/configuration/database-dev.config.ts
@@ -0,0 +1,18 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export function getTypeOrmConfig(): TypeOrmModuleOptions {
+  return {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    type: process.env.DATABASE_TYPE || ('postgres' as any),
+    host: process.env.DATABASE_HOST || '127.0.0.1',
+    port: parseInt(process.env.DATABASE_PORT || '5432', 10),
+    username: process.env.DATABASE_USERNAME || 'admin',
+    password: process.env.DATABASE_PASSWORD || 'adminpassword',
+    database: process.env.DATABASE_NAME || 'bvv-dev',
+    autoLoadEntities: true,
+    synchronize: process.env.NODE_ENV !== 'production',
+  };
+}
+
+// Default export for backward compatibility
+export default getTypeOrmConfig();
