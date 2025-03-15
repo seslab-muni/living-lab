d2640d7fc5f333ede80bc5eea7a059ad31a1fb2b
Patrik Prochazka <me@patrikprochazka.com>
2025-03-15 11:07:59 +0100

    remove ConfigModule for local development

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
 import databaseDevConfig from './configuration/database-dev.config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      envFilePath: '.env',
-    }),
     TypeOrmModule.forRoot(databaseDevConfig),
   ],
   controllers: [],
