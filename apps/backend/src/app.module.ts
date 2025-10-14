904e3d5858290f74480d930d8515a552c805444d
Martin Oslej <matko.oslej@gmial.com>
2025-10-14 11:21:07 +0200

    add schedule modul

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -15,11 +15,13 @@ import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { TenantMiddleware } from './tennant.middleware';
 import { DomainModule } from './domain-role/domain.module';
 import { FacilityModule } from './facility/facility.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot(databaseDevConfig),
+    ScheduleModule.forRoot(),
     UserModule,
     AuthModule,
     OrganizationModule,
